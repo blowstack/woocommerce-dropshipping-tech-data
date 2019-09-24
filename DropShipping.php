9f00e48c5218d278a4e6ee884468cc88cd8160e8
Piotr Golon <piotrgolon@gmail.com>
2019-09-24 08:14:01 +0200

    search engine

--- DropShipping.php
+++ DropShipping.php
@@ -256,6 +256,47 @@ add_action('woocommerce_product_options_general_product_data', 'woocommerce_prod
 // save additional fields
 add_action('woocommerce_process_product_meta', 'woocommerce_product_custom_fields_save');
 
+
+
+// search engine
+function cf_search_join( $join ) {
+  global $wpdb;
+
+  if ( is_search() ) {
+    $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+  }
+
+  return $join;
+}
+add_filter('posts_join', 'cf_search_join' );
+
+function cf_search_where( $where ) {
+  global $pagenow, $wpdb;
+
+  if ( is_search() ) {
+    $where = preg_replace(
+      "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+      "(".$wpdb->posts.".post_title LIKE $1) OR ((".$wpdb->postmeta.".meta_value LIKE $1) and ".$wpdb->postmeta.".meta_key = '_manufacturer_id')", $where );
+  }
+
+  return $where;
+}
+add_filter( 'posts_where', 'cf_search_where' );
+
+function cf_search_distinct( $where ) {
+  global $wpdb;
+
+  if ( is_search() ) {
+    return "DISTINCT";
+  }
+
+  return $where;
+}
+add_filter( 'posts_distinct', 'cf_search_distinct' );
+// search engine
+
+
+
 register_activation_hook(__FILE__, [ $DropShipping , 'install']);
 
 //add_action( 'woocommerce_order_details_after_order_table', 'placeOrderTechData');
