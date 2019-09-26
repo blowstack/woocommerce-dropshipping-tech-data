e79ffd34c9544ec3e161bf03a4815c594446d42f
Piotr Golon <piotrgolon@gmail.com>
2019-09-27 00:04:26 +0200

    WpproductGenerator revised

--- TechDataProductGenerator.php
+++ TechDataProductGenerator.php
@@ -6,103 +6,109 @@
 class TechDataProductGenerator {
 
   private $dropshipping_type;
+  private $WpProductRepository;
 
+  /**
+   * TechDataProductGenerator constructor.
+   * @param $dropshipping_type
+   */
   public function __construct($dropshipping_type) {
     require_once( dirname( __FILE__ ) . '/repos/WpProductRepository.php' );
     $this->dropshipping_type = $dropshipping_type;
+    $this->WpProductRepository = new WpProductRepository();
   }
 
   public function generatePosts() {
     $dropshipping_type = $this->dropshipping_type;
 
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPosts($dropshipping_type);
   }
 
   public function generateWpPostMetasBasic(array $post_metas) {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetasBasic($post_metas);
   }
 
   public function generatePostMetaSku() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaSku();
   }
 
   public function generatePostMetaManufacturer() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaManufacturer();
   }
 
   public function generatePostMetaPrice() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaPrice();
   }
 
   public function generatePostMetaRegularPrice() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaRegularPrice();
   }
 
   public function generatePostMetaStock() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaStock();
   }
 
   public function generatePostMetaProducerCode() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaProducerCode();
   }
 
   public function generatePostMetaBrand() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaBrand();
   }
 
   public function generatePostMetaDropShipping() {
     $dropshipping_type = $this->dropshipping_type;
 
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaDropShipping($dropshipping_type);
   }
 
   public function getTechDataCategories(): array {
     $dropshipping_type = $this->dropshipping_type;
 
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $categories = $WpProductRepository->getTechDataCategories($dropshipping_type);
     return $categories;
   }
 
   public function insertNewCategories($categories) {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->insertNewCategories($categories);
   }
 
   public function generatePostCategories() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateProductType();
     $WpProductRepository->generateProductLang();
     $WpProductRepository->generateProductCategory();
   }
 
   public function generateWpPostMetaCost() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaCost();
   }
 
   public function generateWpPostMetaImage() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->generateWpPostMetaImage();
   }
 
   public function updatePrice() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->updatePrice();
   }
 
   public function updateStock() {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $WpProductRepository->updateStock();
   }
 
@@ -111,7 +117,7 @@ class TechDataProductGenerator {
    * @return array|object|null
    */
   public function getForCSV(): array {
-    $WpProductRepository = new WpProductRepository();
+    $WpProductRepository = $this->WpProductRepository;
     $products = $WpProductRepository->getForCSV();
     return $products;
   }
--- TechDataSynchronizer.php
+++ TechDataSynchronizer.php
@@ -31,8 +31,6 @@ class TechDataSynchronizer {
     }
 
     $this->ProductGenerator = new TechDataProductGenerator($dropshipping_type);
-
-
   }
 
 
